cda4ce22f9b589009949b611c25a30a27ff4b754
Emre Aslan <emreaslan7@icloud.com>
2023-05-11 23:53:51 +0300

    contract & frontend

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ function App() {
 
 
   return (
-    <div className="App">
-      
-      <NavBar accounts={accounts} setAccounts={setAccounts} />
-      <MainMint accounts={accounts} setAccounts={setAccounts} />
-
+    <div className='overlay'>
+      <div className="App">
+        <NavBar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </div>
+      <div className='moving-background'></div>
     </div>
   );
 }
